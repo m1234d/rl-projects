0bc8f9d963b010de6c2233caeddecc3c24f0b490
Matthew de Soto <matthew54213@gmail.com>
2019-05-03 20:58:47 -0400

    Added roms for DeepQ

---
+++ DeepQ/SpaceInvaders/spaceinvaders.py
@@ -0,0 +1,21 @@
+
+
+import tensorflow as tf      # Deep Learning library
+import numpy as np           # Handle matrices
+import retro                 # Retro Environment
+
+
+from skimage import transform # Help us to preprocess the frames
+from skimage.color import rgb2gray # Help us to gray our frames
+
+import matplotlib.pyplot as plt # Display graphs
+
+from collections import deque# Ordered collection with ends
+
+import random
+
+import warnings # This ignore all the warning messages that are normally printed during the training because of skiimage
+warnings.filterwarnings('ignore')
+
+env = retro.make(game='SpaceInvaders-Atari2600')
+env.render()
